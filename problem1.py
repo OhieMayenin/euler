bb9dd39dbb073cb0423b2ea7342a1d591601e7c6
mayenino <2ppyQ468>
2019-09-23 11:24:28 +0100

    Refactored solution to problem 1

--- problem1.py
+++ problem1.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-multiples = []
-sum = 0
-for i in range(1, 1000):
-    if (i % 3 == 0) or (i % 5 == 0):
-        multiples.append(i)
-        sum += i
 
-print(sum)
+def sum(max):
+    sum = 0
+    for i in range(1, max):
+        if (i % 3 == 0) or (i % 5 == 0):
+            sum += i
+    return sum
+
+
+print(sum(1000))
