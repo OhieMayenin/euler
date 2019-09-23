14b9df19b8273b31bb1219e12751a2a8d66673eb
mayenino <2ppyQ468>
2019-09-23 16:36:16 +0100

    Solution to problem 4

--- problem4.py
+++ problem4.py
@@ -1,3 +1,4 @@
+import math
 
 
 def is_palindrome(s):
@@ -10,13 +11,15 @@ def is_palindrome(s):
     return False
 
 
-def largest_palindrome(max):
-    for a in range(max, 0, -1):
-        for b in range(max, a-1, -1):
+def largest_palindrome(limit):
+    palindromes = []
+    for a in range(limit, math.floor(limit/2), -1):
+        for b in range(limit, a-1, -1):
             product = a * b
             print("a=", a, "b=", b, "product=", product)
             if is_palindrome(str(product)):
-                return product
+                palindromes.append(product)
+    return max(palindromes)
 
 
 print(largest_palindrome(999))
